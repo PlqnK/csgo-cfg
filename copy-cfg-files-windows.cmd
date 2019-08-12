5faccb6adbf8cef6b39aaa889ebf26ecad115d3c
Victor Bouvier-Deleau <victor.bouvier-deleau@kolabnow.com>
2019-08-12 19:43:29 +0200

    Remove uneeded slash from local path

--- copy-cfg-files-windows.cmd
+++ copy-cfg-files-windows.cmd
@@ -3,8 +3,8 @@ SETLOCAL ENABLEEXTENSIONS
 
 SET csgo_install_path=%PROGRAMFILES(x86)%\Steam\steamapps\common\Counter-Strike Global Offensive
 
-XCOPY %~dp0\*.cfg "%csgo_install_path%\csgo\cfg"
-XCOPY %~dp0\csgo_textmodorel.txt "%csgo_install_path%\csgo\resource"
+XCOPY %~dp0*.cfg "%csgo_install_path%\csgo\cfg"
+XCOPY %~dp0csgo_textmodorel.txt "%csgo_install_path%\csgo\resource"
 
 PAUSE
 EXIT /B 0
