ea8f310e5ac43c7c95fcfae8a47e66ab8591a7f0
Victor Bouvier-Deleau <victor.bouvier-deleau@kolabnow.com>
2019-08-12 19:42:50 +0200

    Remove quotes in variable value attribution

--- copy-cfg-files-windows.cmd
+++ copy-cfg-files-windows.cmd
@@ -1,7 +1,7 @@
 @ECHO OFF
 SETLOCAL ENABLEEXTENSIONS
 
-SET csgo_install_path="%PROGRAMFILES(x86)%\Steam\steamapps\common\Counter-Strike Global Offensive"
+SET csgo_install_path=%PROGRAMFILES(x86)%\Steam\steamapps\common\Counter-Strike Global Offensive
 
 XCOPY %~dp0\*.cfg "%csgo_install_path%\csgo\cfg"
 XCOPY %~dp0\csgo_textmodorel.txt "%csgo_install_path%\csgo\resource"
