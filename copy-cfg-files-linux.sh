495b4e7da5af88e51dfa3c3a7c73a9654a3e043c
Victor Bouvier-Deleau <victor.bouvier-deleau@kolabnow.com>
2020-04-08 23:31:03 +0200

    Change CS:GO install path

--- copy-cfg-files-linux.sh
+++ copy-cfg-files-linux.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-readonly CSGO_INSTALL_PATH="/opt/SteamLibrary/steamapps/common/Counter-Strike Global Offensive"
+readonly CSGO_INSTALL_PATH="/home/victor/.local/share/Steam/steamapps/common/Counter-Strike Global Offensive"
 
 cp cfg/* "${CSGO_INSTALL_PATH}/csgo/cfg/"
 cp resource/* "${CSGO_INSTALL_PATH}/csgo/resource/"
